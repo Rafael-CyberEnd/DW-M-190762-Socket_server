0d7fab1762100808d993bb551cfeda4d9fe15cf3
Rafael Amador Barrios <rafael.amadorbarrios@utxicotepec.edu.mx>
2022-06-21 13:59:36 -0500

    Primer commit - rest server

---
+++ dist/routes/router.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+exports.router = (0, express_1.Router)();
+exports.router.get('/mensajes', (req, res) => {
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien C:'
+    });
+});
+exports.router.post('/mensajes', (req, res) => {
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    res.json({
+        ok: true,
+        cuerpo: 'POST LISTO',
+        de
+    });
+});
+exports.default = exports.router;
