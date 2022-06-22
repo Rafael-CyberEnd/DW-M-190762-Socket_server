7d2ebfd0575e929cd893358e7dd26c5902b84b93
Rafael Amador Barrios <rafael.amadorbarrios@utxicotepec.edu.mx>
2022-06-22 14:30:39 -0500

    segundo commit: implementacion del patron de diseño singleton en la clase server

--- dist/classes/server.js
+++ dist/classes/server.js
@@ -5,13 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("../global/environment");
-class server {
+const socket_io_1 = __importDefault(require("socket.io"));
+const http_1 = __importDefault(require("http"));
+class Server {
     constructor() {
         this.app = (0, express_1.default)();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
+        this.io = new socket_io_1.default.Server(this.httpServer);
+        this.escucharSockets();
+    }
+    static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+    escucharSockets() {
+        console.log("Escuchando conexiones");
+        this.io.on('conection', cliente => {
+            console.log('cliente conectado');
+        });
     }
     star(callback) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
-exports.default = server;
+exports.default = Server;
